ebc8e87c4a08bc35375a75642a068411c54a5fa3
Ben Sherratt <44046941+btsherratt@users.noreply.github.com>
2023-07-26 21:06:40 +0900

    Added PHP version check
    Also changed quotes

--- public/index.php
+++ public/index.php
@@ -9,9 +9,16 @@
                                        `---'
  */
 
-$main_file = join(DIRECTORY_SEPARATOR, array(__DIR__, "..", "cx", "cx.php"));
-$db_file = join(DIRECTORY_SEPARATOR, array(__DIR__, "..", "db", "btscx.db"));
-$data_folder = join(DIRECTORY_SEPARATOR, array(__DIR__, "..", "data"));
-$public_data_folder = join(DIRECTORY_SEPARATOR, array(__DIR__, "data"));
+define('CX_PHP_VERSION', '8.0.0');
+
+if (version_compare(phpversion(), CX_PHP_VERSION) < 0) {
+    echo('Requires PHP version ' . CX_PHP_VERSION . ' or later');
+    exit;
+}
+
+$main_file = join(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'cx', 'cx.php'));
+$db_file = join(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'db', 'btscx.db'));
+$data_folder = join(DIRECTORY_SEPARATOR, array(__DIR__, '..', 'data'));
+$public_data_folder = join(DIRECTORY_SEPARATOR, array(__DIR__, 'data'));
 require_once($main_file);
 cx($db_file, $data_folder, $public_data_folder);
